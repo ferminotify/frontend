f7b465d0799a386121269da70bd950f2d46d5783
Kevin <kevliujudi@gmail.com>
2026-05-17 11:54:11 +0200

    feat(vite): add ssgOptions for vite-ssg prerender config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+    includedRoutes(paths, routes) {
+      return ['/', '/faq', '/app', '/team', '/supporters', '/archive']
     },
   },
 })
